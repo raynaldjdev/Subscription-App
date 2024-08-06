e6c42d56c9bdb243dd6f505b206c7af7d2d3fbeb
Jean-Pierre <rjpierre@smithcurrie.com>
2024-08-06 12:19:21 -0400

    First Commit

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+
+import styled from "styled-components"
+
+const HeroComponent = styled.header`
+background-color:pink
+
+`
+
+const Hero = ()=>{
+    return <HeroComponent>Hero</HeroComponent>
+};
+export default Hero;
